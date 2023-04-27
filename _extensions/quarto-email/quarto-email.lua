cf6c7dd554ee6f5cb2aae781012672359e4032b6
Richard Iannone <riannone@me.com>
2023-04-27 10:57:31 -0400

    Update quarto-email.lua

--- _extensions/quarto-email/quarto-email.lua
+++ _extensions/quarto-email/quarto-email.lua
@@ -1,14 +1,39 @@
-local subject = nil
-local attachments = nil
 
-function process_document(doc)
-    local str = quarto.json.encode({
-        hello = "world",
-        subject = subject,
-        attachments = attachments
-    })
-    io.open(".connect-email.json", "w"):write(str):close()
-end
+-- package.path = package.path .. ";../luasocket/?.lua;" 
+
+-- local mime = require("mime")
+
+-- local subject = nil
+-- local attachments = nil
+
+-- local headers = {
+--   to = "recipient@example.com",
+--   from = "sender@example.com",
+--   subject = "HTML Email",
+--   ["content-type"] = "text/html; charset=utf-8",
+-- }
+
+-- local body = [[
+--   <html>
+--       <head>
+--           <title>HTML Email</title>
+--       </head>
+--       <body>
+--           <h1>This is an HTML email.</h1>
+--           <p>Welcome to my email message!</p>
+--       </body>
+--   </html>
+-- ]]
+
+-- print(body)
+
+-- Create the email message
+-- local msg = mime.create(headers, body)
+
+-- Print the email message
+-- print(msg)
+
+
 
 function Meta(meta)
     attachments = {}
@@ -24,6 +49,27 @@ function Div(div)
     end
 end
 
+function process_document(doc)
+  local str = quarto.json.encode({
+      hello = "world",
+      subject = subject,
+      attachments = attachments
+  })
+  io.open(".connect-email.json", "w"):write(str):close()
+end
+
 function Pandoc(doc)
+
     process_document(doc)
+
+    -- print JSON document (mostly for development purposes)
+    local json_file = io.open(".connect-email.json", "r")
+    if json_file then
+    local contents = json_file:read("*all")
+    json_file:close()
+    print(contents)
+else
+    print("Error: could not open file")
+end
+
 end
