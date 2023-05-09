e23091c1419f94788eb1831a232895e6e8e5f7b7
Richard Iannone <riannone@me.com>
2023-05-09 13:46:15 -0400

    Update quarto-email.lua

--- _extensions/quarto-email/quarto-email.lua
+++ _extensions/quarto-email/quarto-email.lua
@@ -2,6 +2,7 @@
 
 local subject = nil
 local attachments = nil
+local email_html = nil
 
 -- TODO:
 --   use a parameter to control whether to produce the contents
@@ -9,27 +10,28 @@ local attachments = nil
 --   div as the output
 
 function Meta(meta)
-    attachments = {}
-    for _, v in pairs(meta.attachments) do
-        table.insert(attachments, pandoc.utils.stringify(v))
-    end
+  attachments = {}
+  for _, v in pairs(meta.attachments) do
+    table.insert(attachments, pandoc.utils.stringify(v))
+  end
+
+  -- etc.
+  meta["rsc-report-rendering-url"] = "..."
 end
 
 function Div(div)
   if div.classes:includes("subject") then
-      subject = pandoc.utils.stringify(div)
-      return {}
+    subject = pandoc.utils.stringify(div)
+    print("The email subject is: " .. subject)
+    return {}
   elseif div.classes:includes("email") then
-    local html = extract_div_html(div)
-    print("extracted html")
-    print(html)
-    print(string.len(html))
-    print(type(html))
+    email_html = extract_div_html(div)
+    print("-- Email message HTML has been extracted -- length is " .. string.len(email_html) .. " characters.")
   end
 end
 
 -- function to extract the rendered HTML from a Div of class 'email'
--- this requires a 
+-- this requires a
 function extract_div_html(doc)
   -- local html = ""
   -- local function walk_block(block)
@@ -43,10 +45,15 @@ function extract_div_html(doc)
   -- pandoc.walk_block(doc, {walk_block = walk_block})
   -- return html
 
-  return pandoc.write(pandoc.Pandoc({doc}), "html")
+  return pandoc.write(pandoc.Pandoc({ doc }), "html")
 end
 
 function process_document(doc)
+
+  -- TODO: perform processing on the email HTML
+
+  print("Lines of HTML of email follows:\n" .. email_html)
+
   local str = quarto.json.encode({
     rsc_email_subject = subject,
     rsc_email_attachments = attachments,
@@ -57,24 +64,23 @@ function process_document(doc)
 end
 
 function Pandoc(doc)
-
   -- local rendering_email = get_option() -- TODO
   -- if rendering_email then
-  --   -- make the content of doc be only the content of the .email div    
+  --   -- make the content of doc be only the content of the .email div
   -- else
   --   -- remove the .email div from doc
   -- end
 
-    process_document(doc)
+  process_document(doc)
 
-    -- print JSON document (this is mostly for development purposes)
-    local json_file = io.open("connect-email.json", "r")
-    if json_file then
-        local contents = json_file:read("*all")
-        json_file:close()
-        print(contents)
-    else
-        print("Error: could not open file")
-    end
+  -- print JSON document (this is mostly for development purposes)
+  local json_file = io.open("connect-email.json", "r")
 
+  if json_file then
+    local contents = json_file:read("*all")
+    json_file:close()
+    print(contents)
+  else
+    print("Error: could not open file")
+  end
 end
